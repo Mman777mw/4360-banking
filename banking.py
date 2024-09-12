80b0c4fcd02398051be3ba4ed66f1aabd9aaf39b
Michael W. <mman777mw@yahoo.com>
2024-09-11 22:43:41 -0500

    Update banking.py

--- banking.py
+++ banking.py
@@ -35,19 +35,46 @@ def userLogin():
 				   print(f"Welcome, Commander {user.firstName}. How may we assist you today?")
 				else:
 					print(f"Login successful. Welcome, {user.fullName}.")
+
 				return user
+			else:
+				print("Error: Invalid username or password. Please try again.")
 
 		currentAttempts += 1
-		print("Error: Invalid username or password. Please try again.")
-	
+		
 	print("Extraneous logins detecting. Security closure engaged.")
 	return None
 
+def amountVerify(amountEnt):
+	amVerLoop = True
+	while amVerLoop:
+		confirmAm = input(f"You have entered {amountEnt} is this correct?\nPlease enter Yes or No").strip().lower()
+		if confirmAm in ["yes", "y"]:
+		    return amountEnt
+		elif confirmAm in ["no", "n"]:
+			newAmount = input("Please enter the correct amount: ")
+
+		
+
 def main():
 	currentUser = userLogin()
+	currentOps = 0
 	if currentUser:
-		while True:
-			print(f"Your current balance is {currentUser.currentBalance")
+		while currentOps < 3:
+			print(f"\nYour current balance is {currentUser.currentBalance} M-Corp. Credits.\nWhat would you like to do today?")
+			opSelect = input(f"Your current options are Deposit, Withdraw, Transfer, and  Exit\n").strip().lower()
+
+			if opSelect == "deposit":
+			elif opSelect == "withdraw":
+
+			elif opSelect == "transfer":
+
+			elif opSelect == "exit":
+				print("Logout successful. Have a nice day.")
+				break
+
+			else:
+				print("Invalid choice. Please try again.")
 
 if __name__ == "__main__":
     main()
