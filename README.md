0ce460385727a14c8859e5ff758f8da1b0ddaad6
Michael W. <mman777mw@yahoo.com>
2024-09-11 20:54:10 -0500

    Adding code

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # 4360-banking
+
+Get Started:
+Open banking.py through the Python IDLE shell, then under the Run dropdown on the banking.py window select Run Module.
+
+The user may use existing usernames and passwords within the program to login, or enter their own in the program.
+
+Existing Usernames and Passwords are:
+UserName: "Mman777mw" Password: "M@Corp@Est2010"
+UserName: "Mant1s01" Password: "L3tM3@in"
+UserName: "Meta01" Password: "SerDesSCMCM-01"
+UserName: "Meta04" Password: "100Pr@@f7&62"
+
+To create new users please utilize the following template replacing the brackets [] with the correlating information.
+[User Object Name] = Account_Info("[Username]", "[Password]", "[Current Balance]", "[First Name]", "[Last Name]")
+Best to paste the template below the last existing user and enter the correlating information.
+Do not forget to update userList with the name of the User Object Name, otherwise the login information will not be properly registered in the program.
+
+User interface:
--- banking.py
+++ banking.py
@@ -3,23 +3,39 @@
 #Remind professor about GUI vs Command Line
 class Account_Info:
 	"""docstring for Account_Info"""
-	def __init__(self, userName, password, currentBalance, firstName, lastName):
-		super(Account_Info, self).__init__()
+	def __init__(self, userName, password, currentBalance, accountNum, firstName, lastName):
+
 		self.userName = userName
 		self.password = password
 		self.currentBalance = currentBalance
+		self.accountNum = accountNum
 		self.firstName = firstName
 		self.lastName = lastName
 	
 	def fullName(self):
 		return f"{self.firstName} {self.lastName}"
 
-user1 = Account_Info("Mman777mw", "M@Corp@Est2010", "4500000", "Michael", "Wright")
-user2 = Account_Info("Mant1s01", "L3tM3@in", "5500000", "Michaelle", "Wright")
-user3 = Account_Info("Meta01", "SerDesSCMCM-01", "750000", "Isaac", "MC")
-user4 = Account_Info("Meta04", "100Pr@@f7&62", "200000", "Amish", "MC")
+user1 = Account_Info("Mman777mw", "M@Corp@Est2010", "4500000", "29696", "Michael", "Wright")
+user2 = Account_Info("Mant1s01", "L3tM3@in", "5500000", "62496", "Michaelle", "Wright")
+user3 = Account_Info("Meta01", "SerDesSCMCM-01", "750000", "626601", "Isaac", "MC")
+user4 = Account_Info("Meta04", "100Pr@@f7&62", "200000", "626604", "Amish", "MC")
 
+userList = [user1, user2, user3, user4]
 def userLogin():
+	currentAttempts = 0
+
+	while currentAttempts < 3 :
+		userEntry = input("Username: ")
+		passEntry = input("Password: ")
+
+		for user in userList:
+			if user.userName == userEntry and user.password == passEntry :
+				if user.userName == "Mman777mw":
+				   print(f"Welcome, Commander {user.firstName}. How may we assist you today?")
+				else:
+					print(f"Login successful. Welcome, {user.fullName}.")
+				return True
+			else:
 
 
 def main():
